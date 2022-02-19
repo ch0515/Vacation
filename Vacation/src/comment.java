3a122d019199d118927511a9f09f56a8a6e2ffe7
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-20 03:38:33 +0900

    first commit

---
+++ Vacation/src/comment.java
@@ -0,0 +1,10 @@
+
+public class comment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//이 부분을 주석처리 해서 프로그램이 정상적으로 실행되도록 만들어 보세요.
+        System.out.println("HelloWorld");
+	}
+
+}
