b66a1ca529f9c2438c846597f0e202ee0ae401f0
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-20 16:48:48 +0900

    상수를 사용한 프로그램

---
+++ Vacation/src/ConstantExam.java
@@ -0,0 +1,12 @@
+
+public class ConstantExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14159;
+		int radius = 5;
+		double result = radius * 2 * PI;
+		System.out.println(result);
+	}
+
+}
