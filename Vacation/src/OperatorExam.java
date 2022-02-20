e67b27a6419fcaf01bf024bda9415320681f16b7
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-21 00:25:27 +0900

    산술 연산자

---
+++ Vacation/src/OperatorExam.java
@@ -0,0 +1,21 @@
+
+public class OperatorExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 7;
+		int b = 3;
+		
+		int c = a+b;
+		int d = a-b;
+		int e = a*b;
+		int f = a%b;
+		
+		System.out.printf("C는 : %d\n",c);
+		System.out.printf("D는 : %d\n",d);
+		System.out.printf("E는 : %d\n",e);
+		System.out.printf("F는 : %d\n",f);
+		
+	}
+
+}
