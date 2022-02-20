761aee74c30e61a30cd592eb90b99f6bab743410
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-20 22:48:20 +0900

    기본형 타입

---
+++ Vacation/src/primitiveTest.java
@@ -0,0 +1,17 @@
+
+public class primitiveTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isFun = false;
+		char charValue = 'a';
+		int intValue = 20;
+		long longValue = 2147483648L;
+		
+		System.out.println(isFun);
+		System.out.println(charValue);
+		System.out.println(intValue);
+		System.out.println(longValue);
+	}
+
+}
