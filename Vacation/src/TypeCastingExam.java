032489f6813d81b745a42ebae13cc06bccd58b49
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-20 23:04:46 +0900

    기본형 타입 변환

---
+++ Vacation/src/TypeCastingExam.java
@@ -0,0 +1,11 @@
+
+public class TypeCastingExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intValue = 200;
+		long longValue = intValue;
+		System.out.println(longValue);
+	}
+
+}
---
+++ Vacation/src/TypeCastingExam2.java
@@ -0,0 +1,11 @@
+
+public class TypeCastingExam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long longValue = 20;
+		int intValue = (int) longValue;
+		System.out.println(intValue);
+	}
+
+}
