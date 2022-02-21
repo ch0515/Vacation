0271a523908406e1ac128ac3c9054eef3163eab7
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-21 19:22:29 +0900

    while문

---
+++ Vacation/src/WhileExam.java
@@ -0,0 +1,13 @@
+
+public class WhileExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		while(i<11) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
