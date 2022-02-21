4238a9c6fc89f2bda9f335d22c0b8325c7f2bd62
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-21 18:33:29 +0900

    비교 연산자

---
+++ Vacation/src/relation_operator.java
@@ -0,0 +1,18 @@
+
+public class relation_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 10;
+		
+		boolean c = a > b;
+		boolean d = a != b;
+		boolean e = a == b;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+
+}
