b78d34009c7b863857c5fb6f9b24445c81fa7803
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-02-21 19:48:45 +0900

    배열

---
+++ Vacation/src/ArrayExam2.java
@@ -0,0 +1,13 @@
+
+public class ArrayExam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[100];
+		for(int i=0; i<= 99; i++) {
+			array[i] = i+1;
+			System.out.println(array[i]);
+		}
+	}
+
+}
---
+++ Vacation/src/ForEachExam.java
@@ -0,0 +1,11 @@
+
+public class ForEachExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] array = {1,5,2,6,7};
+		for(int i:array) {
+			System.out.println(i);
+		}
+	}
+}
